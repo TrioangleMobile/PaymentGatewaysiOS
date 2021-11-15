8d69c010f2511c727873e012ebee9fc1bc0596e5
sivasonai <sivasonai@trioangle.com>
2021-11-15 15:30:05 +0530

    Pod lib tested -> Karuppasamy

--- PaymentHelper/Source/BrintreeHandler.swift
+++ PaymentHelper/Source/BrintreeHandler.swift
@@ -7,9 +7,8 @@
 //
 
 import Foundation
-import Braintree
-import BraintreeDropIn
-
+//import Braintree
+//import BraintreeDropIn
 
 public
 enum BTErrors : Error{
@@ -27,200 +26,200 @@ extension BTErrors : LocalizedError {
 
 public
 protocol BrainTreeProtocol {
-    
+
     func initalizeClient(with id : String)
-    
-    func authenticatePaymentUsing(_ view : UIViewController,
-                                  threeDSecureRequest : BTThreeDSecureRequest,
-                                  address : BTThreeDSecurePostalAddress,
-                                  for amount : Double,
-                                  result: @escaping BrainTreeHandler.BTResult)
-    func authenticatePaypalUsing(_ view : UIViewController,
-                                  for amount : Double,
-                                  currency: String,
-                                  result: @escaping BrainTreeHandler.BTResult)
+//
+//    func authenticatePaymentUsing(_ view : UIViewController,
+//                                  threeDSecureRequest : BTThreeDSecureRequest,
+//                                  address : BTThreeDSecurePostalAddress,
+//                                  for amount : Double,
+//                                  result: @escaping BrainTreeHandler.BTResult)
+//    func authenticatePaypalUsing(_ view : UIViewController,
+//                                  for amount : Double,
+//                                  currency: String,
+//                                  result: @escaping BrainTreeHandler.BTResult)
 }
 
 open
 class BrainTreeHandler : NSObject {
-    static var ReturnURL  : String  {
-        let bundle = Bundle.main
-        return bundle.bundleIdentifier ?? "comg.trioangle.gofer"
-    }
-    open
-    class func isBrainTreeHandleURL(_ url: URL,
-                                    options: [UIApplication.OpenURLOptionsKey : Any]) -> Bool{
-        if url.scheme?
-            .localizedCaseInsensitiveCompare(BrainTreeHandler.ReturnURL) == .orderedSame {
-            return BTAppContextSwitcher.handleOpenURL(url)
-        }
-        return false
-    }
-    public
-    typealias BTResult = (Result<BTPaymentMethodNonce, Error>) -> Void
-    static var `default` : BrainTreeProtocol = {
-        BrainTreeHandler()
-    }()
-    
-    var client : BTAPIClient?
-    var hostView : UIViewController?
-    var result : BTResult?
-    var clientToken : String?
-    private override init(){
-        super.init()
-        
-    }
-    
-}
-
-//MARK:- BrainTreeProtocol
-extension BrainTreeHandler : BrainTreeProtocol{
-    
-    public func initalizeClient(with id : String){
-        self.clientToken = id
-        self.client = BTAPIClient(authorization: id)
-        BTAppContextSwitcher.setReturnURLScheme(BrainTreeHandler.ReturnURL)
-    }
-    
-    public func authenticatePaypalUsing(_ view: UIViewController,
-                                  for amount: Double,
-                                  currency: String,
-                                  result: @escaping BrainTreeHandler.BTResult) {
-        guard let currentClient = self.client else{
-            result(.failure(BTErrors.clientNotInitialized))
-            return
-        }
-        self.hostView = view
-        self.result = result
-        let paypalDriver = BTPayPalDriver(apiClient: currentClient)
-        let request = BTPayPalCheckoutRequest(amount: amount.description)
-        request.currencyCode = currency
-        paypalDriver.tokenizePayPalAccount(with: request) { (payPalAccountNonce, error) in
-            guard let paypaNonce = payPalAccountNonce else{
-                result(.failure(error ?? BTErrors.clientCancelled))
-                return
-            }
-            print(paypaNonce.email ?? "")
-            print(paypaNonce.firstName ?? "")
-            print(paypaNonce.nonce)
-            result(.success(paypaNonce))
-        }
-    }
-    
-    public
-    func authenticatePaymentUsing(_ view : UIViewController,
-                                  threeDSecureRequest : BTThreeDSecureRequest,
-                                  address : BTThreeDSecurePostalAddress,
-                                  for amount : Double,
-                                  result: @escaping BTResult) {
-        guard let currentClientToken = self.clientToken else{
-            result(.failure(BTErrors.clientNotInitialized))
-            return
-        }
-        self.hostView = view
-        self.result = result
-        
-        
-        _ = BTDropInRequest()
-//        let threeDSecureRequest = BTThreeDSecureRequest()
-//        threeDSecureRequest.amount = NSDecimalNumber(value: amount)
-//        threeDSecureRequest.email = ""//UserDefaults.value(for: .user_email_id) ?? "test@email.com" as! String
-//        threeDSecureRequest.versionRequested = .version2
+//    static var ReturnURL  : String  {
+//        let bundle = Bundle.main
+//        return bundle.bundleIdentifier ?? "comg.trioangle.gofer"
+//    }
+//    open
+//    class func isBrainTreeHandleURL(_ url: URL,
+//                                    options: [UIApplication.OpenURLOptionsKey : Any]) -> Bool{
+//        if url.scheme?
+//            .localizedCaseInsensitiveCompare(BrainTreeHandler.ReturnURL) == .orderedSame {
+//            return BTAppContextSwitcher.handleOpenURL(url)
+//        }
+//        return false
+//    }
+//    public
+//    typealias BTResult = (Result<BTPaymentMethodNonce, Error>) -> Void
+//    static var `default` : BrainTreeProtocol = {
+//        BrainTreeHandler()
+//    }()
 //
-//        let address = BTThreeDSecurePostalAddress()
-//        address.givenName = ""// UserDefaults.value(for: .first_name) ?? "Albin" as! String // ASCII-printable characters required, else will throw a validation error
-//        address.surname = ""//UserDefaults.value(for: .last_name) ?? "MrngStar" as! String // ASCII-printable characters required, else will throw a validation error
-//        address.phoneNumber = ""//UserDefaults.value(for: .phonenumber) ?? "123456" as! String
+//    var client : BTAPIClient?
+//    var hostView : UIViewController?
+//    var result : BTResult?
+//    var clientToken : String?
+//    private override init(){
+//        super.init()
+//
+//    }
+//
+}
+//
+////MARK:- BrainTreeProtocol
+//extension BrainTreeHandler : BrainTreeProtocol{
+//
+//    public func initalizeClient(with id : String){
+//        self.clientToken = id
+//        self.client = BTAPIClient(authorization: id)
+//        BTAppContextSwitcher.setReturnURLScheme(BrainTreeHandler.ReturnURL)
+//    }
+//
+//    public func authenticatePaypalUsing(_ view: UIViewController,
+//                                  for amount: Double,
+//                                  currency: String,
+//                                  result: @escaping BrainTreeHandler.BTResult) {
+//        guard let currentClient = self.client else{
+//            result(.failure(BTErrors.clientNotInitialized))
+//            return
+//        }
+//        self.hostView = view
+//        self.result = result
+//        let paypalDriver = BTPayPalDriver(apiClient: currentClient)
+//        let request = BTPayPalCheckoutRequest(amount: amount.description)
+//        request.currencyCode = currency
+//        paypalDriver.tokenizePayPalAccount(with: request) { (payPalAccountNonce, error) in
+//            guard let paypaNonce = payPalAccountNonce else{
+//                result(.failure(error ?? BTErrors.clientCancelled))
+//                return
+//            }
+//            print(paypaNonce.email ?? "")
+//            print(paypaNonce.firstName ?? "")
+//            print(paypaNonce.nonce)
+//            result(.success(paypaNonce))
+//        }
+//    }
+//
+//    public
+//    func authenticatePaymentUsing(_ view : UIViewController,
+//                                  threeDSecureRequest : BTThreeDSecureRequest,
+//                                  address : BTThreeDSecurePostalAddress,
+//                                  for amount : Double,
+//                                  result: @escaping BTResult) {
+//        guard let currentClientToken = self.clientToken else{
+//            result(.failure(BTErrors.clientNotInitialized))
+//            return
+//        }
+//        self.hostView = view
+//        self.result = result
 //
 //
-//        threeDSecureRequest.billingAddress = address
+//        _ = BTDropInRequest()
+////        let threeDSecureRequest = BTThreeDSecureRequest()
+////        threeDSecureRequest.amount = NSDecimalNumber(value: amount)
+////        threeDSecureRequest.email = ""//UserDefaults.value(for: .user_email_id) ?? "test@email.com" as! String
+////        threeDSecureRequest.versionRequested = .version2
+////
+////        let address = BTThreeDSecurePostalAddress()
+////        address.givenName = ""// UserDefaults.value(for: .first_name) ?? "Albin" as! String // ASCII-printable characters required, else will throw a validation error
+////        address.surname = ""//UserDefaults.value(for: .last_name) ?? "MrngStar" as! String // ASCII-printable characters required, else will throw a validation error
+////        address.phoneNumber = ""//UserDefaults.value(for: .phonenumber) ?? "123456" as! String
+////
+////
+////        threeDSecureRequest.billingAddress = address
+////
+////        // Optional additional information.
+////        // For best results, provide as many of these elements as possible.
+//        let info = BTThreeDSecureAdditionalInformation()
+//        info.shippingAddress = address
+//        threeDSecureRequest.additionalInformation = info
 //
-//        // Optional additional information.
-//        // For best results, provide as many of these elements as possible.
-        let info = BTThreeDSecureAdditionalInformation()
-        info.shippingAddress = address
-        threeDSecureRequest.additionalInformation = info
-
-        let dropInRequest = BTDropInRequest()
-        dropInRequest.threeDSecureRequest = threeDSecureRequest
-        
-        let _dropIn = BTDropInController(authorization: currentClientToken,
-                                         request: dropInRequest) { (controller, result, error) in
-            if let btError = error {
-                // Handle error
-                
-                self.result?(.failure(btError))
-                self.dismissPresentedView()
-            } else if (result?.isCanceled == true) {
-                // Handle user cancelled flow
-                
-                self.result?(.failure(BTErrors.clientCancelled))
-                self.dismissPresentedView()
-            } else if let nonce = result?.paymentMethod{
-                self.result?(.success(nonce))
-                // Use the nonce returned in `result.paymentMethod`
-            }
-            
-            controller.presentedViewController?.dismiss(animated: true,
-                                                        completion: nil)
-            controller.dismiss(animated: true,
-                               completion: nil)
-        }
-        guard let dropIn = _dropIn else{return}
-        view.present(dropIn, animated: true,
-                     completion: nil)
-    }
-}
-//MARK:- BTDropInViewControllerDelegate
-extension BrainTreeHandler : BTDropInControllerDelegate{
-    public func reloadDropInData() { }
-
-    public func editPaymentMethods(_ sender: Any) {
-        
-    }
-    
-    func drop(_ viewController: BTDropInController,
-              didSucceedWithTokenization paymentMethodNonce: BTPaymentMethodNonce) {
-        viewController.presentedViewController?.dismiss(animated: true,
-                                                        completion: nil)
-        self.result?(.success(paymentMethodNonce))
-        self.dismissPresentedView()
-    }
-    
-    func drop(inViewControllerDidCancel viewController: BTDropInController) {
-        viewController.presentedViewController?.dismiss(animated: true,
-                                                        completion: nil)
-        self.result?(.failure(BTErrors.clientCancelled))
-        self.dismissPresentedView()
-    }
-    
-    
-}
-//MARK:- UDF
-extension BrainTreeHandler {
-    
-    @objc
-    func userDidCancelPayment() {
-        self.result?(.failure(BTErrors.clientCancelled))
-        self.dismissPresentedView()
-    }
-    
-    open
-    func dismissPresentedView() {
-        self.hostView?.dismiss(animated: true,
-                               completion: nil)
-    }
-}
-
-extension BrainTreeHandler : BTViewControllerPresentingDelegate{
-    
-    public func paymentDriver(_ driver: Any,
-                       requestsPresentationOf viewController: UIViewController) {
-        
-    }
- 
-    public func paymentDriver(_ driver: Any,
-                       requestsDismissalOf viewController: UIViewController) {
-        
-    }
-}
+//        let dropInRequest = BTDropInRequest()
+//        dropInRequest.threeDSecureRequest = threeDSecureRequest
+//
+//        let _dropIn = BTDropInController(authorization: currentClientToken,
+//                                         request: dropInRequest) { (controller, result, error) in
+//            if let btError = error {
+//                // Handle error
+//
+//                self.result?(.failure(btError))
+//                self.dismissPresentedView()
+//            } else if (result?.isCanceled == true) {
+//                // Handle user cancelled flow
+//
+//                self.result?(.failure(BTErrors.clientCancelled))
+//                self.dismissPresentedView()
+//            } else if let nonce = result?.paymentMethod{
+//                self.result?(.success(nonce))
+//                // Use the nonce returned in `result.paymentMethod`
+//            }
+//
+//            controller.presentedViewController?.dismiss(animated: true,
+//                                                        completion: nil)
+//            controller.dismiss(animated: true,
+//                               completion: nil)
+//        }
+//        guard let dropIn = _dropIn else{return}
+//        view.present(dropIn, animated: true,
+//                     completion: nil)
+//    }
+//}
+////MARK:- BTDropInViewControllerDelegate
+//extension BrainTreeHandler : BTDropInControllerDelegate{
+//    public func reloadDropInData() { }
+//
+//    public func editPaymentMethods(_ sender: Any) {
+//
+//    }
+//
+//    func drop(_ viewController: BTDropInController,
+//              didSucceedWithTokenization paymentMethodNonce: BTPaymentMethodNonce) {
+//        viewController.presentedViewController?.dismiss(animated: true,
+//                                                        completion: nil)
+//        self.result?(.success(paymentMethodNonce))
+//        self.dismissPresentedView()
+//    }
+//
+//    func drop(inViewControllerDidCancel viewController: BTDropInController) {
+//        viewController.presentedViewController?.dismiss(animated: true,
+//                                                        completion: nil)
+//        self.result?(.failure(BTErrors.clientCancelled))
+//        self.dismissPresentedView()
+//    }
+//
+//
+//}
+////MARK:- UDF
+//extension BrainTreeHandler {
+//
+//    @objc
+//    func userDidCancelPayment() {
+//        self.result?(.failure(BTErrors.clientCancelled))
+//        self.dismissPresentedView()
+//    }
+//
+//    open
+//    func dismissPresentedView() {
+//        self.hostView?.dismiss(animated: true,
+//                               completion: nil)
+//    }
+//}
+//
+//extension BrainTreeHandler : BTViewControllerPresentingDelegate{
+//
+//    public func paymentDriver(_ driver: Any,
+//                       requestsPresentationOf viewController: UIViewController) {
+//
+//    }
+//
+//    public func paymentDriver(_ driver: Any,
+//                       requestsDismissalOf viewController: UIViewController) {
+//
+//    }
+//}
--- Podfile
+++ Podfile
@@ -1,13 +1,11 @@
 # Uncomment the next line to define a global platform for your project
-platform :ios, '13.0'
+# platform :ios, '9.0'
 
 target 'PaymentHelper' do
   # Comment the next line if you don't want to use dynamic frameworks
   use_frameworks!
-  project 'PaymentHelper.xcodeproj'
+
   # Pods for PaymentHelper
-    pod 'Braintree'
-    pod 'BraintreeDropIn'
 
   target 'PaymentHelperTests' do
     # Pods for testing
